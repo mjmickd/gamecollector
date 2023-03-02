d8961ac4806032a1a0cd2b93d6fb9efed6860501
mjmickd <mjmickd@gmail.com>
2023-03-02 11:31:38 -0800

    Part 1 Complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+#baby step - Usually a Model is used instead 
+games = [
+  {'name': 'Mario Kart 8 Deluxe', 'system': 'Nintendo Switch', 'description': 'racing game with fun tricks and twists', 'released': 2017},
+  {'name': 'Luigi''s Mansion', 'system': 'Nintendo Gamecube', 'description': 'ghostbusters meets Nintendo', 'released': 2003},
+]
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def games_index(request):
+    return render(request, 'games/index.html', {
+        'games': games
+    })
